4105a339a00f100c964358a287e9ff756f1c8fb0
jimshao <b07901081@ntu.edu.tw>
2022-01-15 23:25:16 +0800

    fix bugs

--- frontend/src/Containers/Login.js
+++ frontend/src/Containers/Login.js
@@ -56,7 +56,7 @@ const Login = ({ setToken, displayStatus}) => {
                         <SignUpSubtitle>Password</SignUpSubtitle>
                         <SignUpFormInput name="password" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                     </SignUpWrapper>                                
-                    <Link to={(!data)? '#': ((!data.account)? "#": `/allpost/?id=${data.account.id}`)} style={{padding: "10px 20px"}} > {/*userpage*/}
+                    <Link to={ (!data)? '#': ((!data.account)? "#": `/allpost/?id=${data.account.id}`)} style={{padding: "10px 20px"}} > {/*userpage*/}
                         <Button onClick={loginHandler} primary fontBig big>Login</Button>
                     </Link>
                 </SignUpForm>
