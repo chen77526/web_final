50e441ba98e538c0c8272b92f62b0a0209f550a6
陳嘉宏 <chen77526@gmail.com>
2022-01-20 17:14:09 +0800

    QQ

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, split } from "@apollo/client";
 import { getMainDefinition } from "@apollo/client/utilities";
 import { WebSocketLink } from "@apollo/client/link/ws";
+const url = new URL("/graphql", window.location.href);
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:5000/',
+  // uri: 'http://localhost:5000/',
+  uri: url.href,
 });
 
 const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:5000/',
+  // uri: 'ws://localhost:5000/',
+  uri: url.href.replace("http", "ws"),
   options: { reconnect: true },
 });
 
